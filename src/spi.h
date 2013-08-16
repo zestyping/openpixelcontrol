37dd64ae041798e4e56818a4f805d27a135a5cbc
Ka-Ping Yee <ping@zesty.ca>
2013-08-16 14:29:17 -0700

    Reassign copyright; Google relinquishes ownership.
    Google has agreed to release any copyright rights it may have
    to Open Pixel Control.

--- src/spi.h
+++ src/spi.h
@@ -1,3 +1,14 @@
+/* Copyright 2013 Ka-Ping Yee
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not
+use this file except in compliance with the License.  You may obtain a copy
+of the License at: http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations under the License. */
+
 #ifndef SPI_H
 #define SPI_H
 
@@ -24,6 +35,5 @@ void spi_transfer(int fd, u32 spi_speed_hz, u8* tx, u8* rx, u32 len, u16 delay);
 void spi_write(int fd, u8* tx, u32 len);
 
 int init_spidev(char dev[], u32 spi_speed_hz);
-
 
 #endif /* SPI_H */
