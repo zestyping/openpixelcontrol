da7a94854f721e41b431db75e6f3f13788fff0ee
Mikkel Kirkgaard Nielsen <miki@vestergaardcompany.com>
2017-01-13 18:47:02 +0100

    Correct usage text
    Add -p necessary when specifying port parameter.
    Remove unneeded <options>.

--- src/gl_server.c
+++ src/gl_server.c
@@ -403,7 +403,7 @@ void init(char** filenames, int total_channels) {
 }
 
 void usage(char* prog_name) {
-  fprintf(stderr, "Usage: %s <options> -l <filename.json> [<port>]\n", prog_name);
+  fprintf(stderr, "Usage: %s -l <filename.json> [-p <port>]\n", prog_name);
   exit(1);
 }
 
