5b0e60d3c140af28960c24fa66e1c64439069ac0
Ka-Ping Yee <ping@zesty.ca>
2017-03-24 14:14:32 -0400

    Fix height calculation in make_cylinder.py.

--- layouts/make_cylinder.py
+++ layouts/make_cylinder.py
@@ -9,30 +9,30 @@ import sys
 #-------------------------------------------------------------------------------
 # command line
 
-parser = optparse.OptionParser(description="""Creates a cylinder
-on the z axis extending from -height/2 to height/2.
-n_tall is optional -- it will default to a value that
-creates square pixels.  You can also create circles by setting
-height to 0.
-""")
+parser = optparse.OptionParser(description='''
+Creates a cylinder along the z-axis extending from -height/2 to height/2.
+n_tall is optional; it will default to a value that creates square pixels.
+You can also create circles by setting height to 0.
+''')
 parser.add_option('--radius', dest='radius', default=1,
-                    action='store', type='float',
-                    help='radius of cylinder. default = 1')
+                  action='store', type='float',
+                  help='radius of cylinder (default 1)')
 parser.add_option('--height', dest='height', default=1,
-                    action='store', type='float',
-                    help='height of cylinder.  default = 1')
+                  action='store', type='float',
+                  help='height of cylinder (default 1)')
 parser.add_option('--n_around', dest='n_around', default=32,
-                    action='store', type='int',
-                    help='number of pixels around the circumference.  default = 32')
+                  action='store', type='int',
+                  help='number of pixels around the circumference (default 32)')
 parser.add_option('--n_tall', dest='n_tall',
-                    action='store', type='int',
-                    help='number of pixels from top to bottom. (optional)')
+                  action='store', type='int',
+                  help='number of pixels from top to bottom (optional)')
 options, args = parser.parse_args()
 
 # figure out how many pixels are needed around the cylinder
 # in order to get square pixels
 if not options.n_tall:
-    options.n_tall = int(options.n_around * options.height / options.radius / math.pi)
+    circumference = options.radius * 2 * math.pi
+    options.n_tall = int(options.n_around * options.height / circumference)
 
 options.n_tall = max(1, options.n_tall)
 
@@ -40,15 +40,14 @@ options.n_tall = max(1, options.n_tall)
 # make cylinder
 
 result = ['[']
-for ii in range(options.n_tall):
-    if options.n_tall == 1:
-        z = 0
-    else:
-        z = (ii / (options.n_tall-1)) * 2 - 1
-    z *= options.height
+z_min = -0.5*options.height
+z_inc = options.height / max(options.n_tall - 1, 1)
 
-    for jj in range(options.n_around):
-        theta = jj / options.n_around * math.pi * 2
+for i in range(options.n_tall):
+    z = z_min + i*z_inc
+
+    for j in range(options.n_around):
+        theta = j / options.n_around * math.pi * 2
         x = math.sin(theta) * options.radius
         y = math.cos(theta) * options.radius
 
@@ -58,7 +57,7 @@ for ii in range(options.n_tall):
 result[-1] = result[-1][:-1]
 
 result.append(']')
-print '\n'.join(result)
+print('\n'.join(result))
 
 sys.stderr.write('\nn_around = %s\n' % options.n_around)
 sys.stderr.write('n_tall = %s\n' % options.n_tall)
